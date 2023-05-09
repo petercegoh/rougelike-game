124030112f1dc5704580d249559249f3dd2f72e4
petercegoh <petercegoh@gmail.com>
2023-05-09 11:56:51 +0800

    Part 2 done

--- tile_types.py
+++ tile_types.py
@@ -14,11 +14,12 @@ graphic_dt = np.dtype(
 # Tile struct used for statically defined tile data.
 tile_dt = np.dtype(
     [
-        ("walkable", np.bool),  # True if this tile can be walked over.
-        ("transparent", np.bool),  # True if this tile doesn't block FOV.
+        ("walkable", np.bool_),  # True if this tile can be walked over.
+        ("transparent", np.bool_),  # True if this tile doesn't block FOV.
         ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
     ]
 )
+
 
 def new_tile(
     *,  # Enforce the use of keywords, so that parameter order doesn't matter.
